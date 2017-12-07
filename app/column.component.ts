e251f8931e809cb6770d5f3790bbd3d371ca2ae0
Tyler Coles <tyler.coles@thomsonreuters.com>
2017-12-07 08:18:08 -0500

    Updated the window to use column component

--- app/column.component.ts
+++ app/column.component.ts
@@ -5,9 +5,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'column',
     template: `
-    <ng-content select="header"></ng-content>
-    <ng-content select="content"></ng-content>
-    <ng-content select="footer"></ng-content>
+    <ng-content></ng-content>
     `
 })
 export class ColumnComponent {
